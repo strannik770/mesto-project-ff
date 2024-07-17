953e802b6b4af4fa746eef1a1ed33d6e2697b6d2
Nikita Poboinev <id90820077@yandex.ru>
2024-07-17 17:15:33 +0300

    full

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/strannik770/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+createCardList(initialCards);
+
+function createCardList(initialCards) {
+    const cardList = document.querySelector(".places__list");
+    for (let obj of initialCards) {
+      createCard(obj,cardList,deleteCard);
+    }
+  }
+
+function createCard(cardValue,cardList,deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = cardValue.link;
+  cardElement.querySelector(".card__title").textContent = cardValue.name;
+  cardDeleteButton.addEventListener("click",deleteCard);
+
+  cardList.append(cardElement);
+}
+
+function deleteCard(event) {
+    event.target.closest(".card").remove();
+}
+
+
